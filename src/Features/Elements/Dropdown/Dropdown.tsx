edc6f01541398e4fdcb85442b3a4c6083cf7b4d1
hackey9 <kazakevich1996@ya.ru>
2021-02-08 19:58:33 +0700

    Add dropdown functionality

--- src/Features/Elements/Dropdown/Dropdown.tsx
+++ src/Features/Elements/Dropdown/Dropdown.tsx
@@ -1,6 +1,5 @@
-import React, {FC, PropsWithChildren} from "react"
+import React, {FC, PropsWithChildren, useCallback, useEffect, useMemo, useReducer, useRef} from "react"
 import css from "./Dropdown.module.scss"
-
 
 
 export type DropdownProps = PropsWithChildren<{
@@ -11,13 +10,46 @@ export type DropdownProps = PropsWithChildren<{
 
 const Dropdown: FC<DropdownProps> = ({value, onChange, data}) => {
 
-  // TODO: implement dropdown
-
   const current = data[value]
+  const items = useMemo(() => Object.entries(data), [data])
+
+  const [isOpen, toggle] = useReducer((s: boolean) => !s, false)
+
+  const handleSelect = useCallback((key: string) => {
+    onChange?.(key)
+
+    isOpen && toggle()
+  }, [isOpen, onChange])
+
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (isOpen) {
+      const closer = (e: MouseEvent) => {
+        e.stopPropagation()
+        // try to find box element in event path
+        if (e.composedPath().indexOf(ref.current!) === -1) {
+          // click was outside of the box
+          toggle()
+        }
+      }
+
+      document.addEventListener("click", closer)
+      return () => document.removeEventListener("click", closer)
+    }
+  }, [isOpen])
+
 
   return (
-    <div className={css.box}>
-      <div className={css.value}>{current}</div>
+    <div className={css.box} ref={ref}>
+      <div className={css.value} onClick={toggle}>{current}</div>
+      {isOpen && (
+        <div className={css.list}>
+          {items.map(([key, name]) => (
+            <div key={key} className={css.listItem} onClick={() => handleSelect(key)}>{name}</div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
